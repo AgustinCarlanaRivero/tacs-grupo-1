21e8dc2821c6dc4044b2412f4d6627a119abc567
Franco Zanier <fzanier@frba.utn.edu.ar>
2026-04-09 12:44:04 -0300

    Borro carpeta routes

--- backend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-const router = Router()
-
-// Los módulos se irán registrando acá a medida que se implementen:
-// import authRoutes from '../modules/auth/auth.routes.ts'
-// router.use('/auth', authRoutes)
-
-export default router
