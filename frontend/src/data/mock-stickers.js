dde1c39ff673287c83fde103b3ca538abb736ea8
gianlucca bolocco <gianluccabolocco@gmail.com>
2026-04-26 13:34:53 -0300

    stickers

---
+++ frontend/src/data/mock-stickers.js
@@ -0,0 +1,74 @@
+export const mockStickers = [
+  {
+    sticker: {
+      number: 10,
+      player: {
+        name: "Christian Pulisic",
+        nationalTeam: { name: "USA" },
+        club: { name: "AC Milan" }
+      },
+      category: "REGULAR"
+    },
+    quantity: 2
+  },
+  {
+    sticker: {
+      number: 45,
+      player: {
+        name: "Alphonso Davies",
+        nationalTeam: { name: "Canada" },
+        club: { name: "Bayern Munich" }
+      },
+      category: "SHINY"
+    },
+    quantity: 1
+  },
+  {
+    sticker: {
+      number: 88,
+      player: {
+        name: "Santiago Giménez",
+        nationalTeam: { name: "Mexico" },
+        club: { name: "Feyenoord" }
+      },
+      category: "REGULAR"
+    },
+    quantity: 3
+  },
+  {
+    sticker: {
+      number: 12,
+      player: {
+        name: "Weston McKennie",
+        nationalTeam: { name: "USA" },
+        club: { name: "Juventus" }
+      },
+      category: "REGULAR"
+    },
+    quantity: 0
+  },
+  {
+    sticker: {
+      number: 99,
+      player: {
+        name: "Guillermo Ochoa",
+        nationalTeam: { name: "Mexico" },
+        club: { name: "Salernitana" }
+      },
+      category: "SHINY"
+    },
+    quantity: 1
+  },
+  {
+    sticker: {
+      number: 23,
+      player: {
+        name: "Jonathan David",
+        nationalTeam: { name: "Canada" },
+        club: { name: "Lille OSC" }
+      },
+      category: "REGULAR"
+    },
+    quantity: 5
+  }
+];
