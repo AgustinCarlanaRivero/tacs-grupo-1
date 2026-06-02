d690259cc3f4e044a85fcb085a9d84e0246f90bc
fzanier <fzanier@frba.utn.edu.ar>
2026-06-02 14:29:45 -0300

    Fix: CORS para deploy

--- backend/src/app/app.ts
+++ backend/src/app/app.ts
@@ -3,29 +3,30 @@ import express from "express";
 import healthRoutes from "../infra/health/health.routes";
 import swaggerRoutes from "../infra/swagger/swagger.routes";
 import apiRoutes from "../routes/index";
-import { errorHandler } from "../shared/middleware/error.middleware";
 import { ForbiddenError } from "../shared/errors/http-errors";
+import { errorHandler } from "../shared/middleware/error.middleware";
 
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://stickersfront.matenote.app",
+];
 
 app.use(
   cors({
     origin: (origin, callback) => {
       if (!origin) return callback(null, true);
-
-      if (
-        origin.endsWith(".netlify.app") ||
-        origin.startsWith("http://localhost")
-      ) {
+      if (allowedOrigins.includes(origin) || origin.endsWith(".netlify.app")) {
         return callback(null, true);
       }
-
       return callback(new ForbiddenError("Not allowed by CORS"));
     },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.use(express.json());
 
